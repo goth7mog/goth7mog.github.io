75676cb7a7cac9cd59a528aaa76d4aeb71146b10
Alexander <kuzurik797@gmail.com>
2018-11-07 08:04:20 +0300

    Commit fix

--- Internet_store/js/common.js
+++ Internet_store/js/common.js
@@ -157,7 +157,19 @@ $("#form").submit(function() {
 
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
-
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
 	
 });
 
